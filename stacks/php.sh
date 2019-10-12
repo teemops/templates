7d7f3d81affe87b0967ce6804bb810e4bc56ffd5
benfell <ben@teemops.com>
2019-10-12 13:49:53 +1300

    force apt yes

--- stacks/php.sh
+++ stacks/php.sh
@@ -2,4 +2,4 @@
 
 #Installs latest PHP 7 on Ubuntu 18
 apt-get update
-apt-get install -y php composer mysql-client php-mysql php-gd php-mbstring php-cli php-common php-curl php-intl php-jmespath php-json php-memcache php-mongodb 
+apt-get -y install php composer mysql-client php-mysql php-gd php-mbstring php-cli php-common php-curl php-intl php-jmespath php-json php-memcache php-mongodb 
