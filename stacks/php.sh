bc4f77a6259d024e6793754cda507b85d5965108
benfell <ben@teemops.com>
2019-10-14 20:20:34 +1300

    Added code deploy agent to php.sh

--- stacks/php.sh
+++ stacks/php.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+apt-get update
+apt-get -y install ruby wget
+mkdir /root/codedeploy
+cwd /root/codedeploy
+wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
 
 #Installs latest PHP 7 on Ubuntu 18
-apt-get update
 apt-get -y install php composer mysql-client php-mysql php-gd php-mbstring php-cli php-common php-curl php-intl php-jmespath php-json php-memcache php-mongodb 
+
+#restart code deploy agent
+service codedeploy-agent restart
