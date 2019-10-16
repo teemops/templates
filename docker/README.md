6b5009464be478dae09a3386e9f1ef5702d9b1b1
Ben Fellows <ben@teem.nz>
2019-10-17 10:52:47 +1300

    added dockerfiles for testing

---
+++ docker/README.md
@@ -0,0 +1,3 @@
+Custom docker images.
+
+These are only required for CodeBuild where the standard image is not supported. They could be used in the future as deployed environments as well.
---
+++ scripts/docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#BASE DOCKER INSTALL FOR ALL DOCKER IMAGES
+#Deploys all necessary TeemOps related configuration for Teemops Docker Containers
+apt-get update
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
+
+#Install standard ansible playbooks
+mkdir /srv
+mkdir /srv/scripts
+git clone https://github.com/teemops/templates.git /srv/scripts/templates
+cd /srv/scripts/templates
+
+# Run default OS configuration
+#TODO
+ansible-playbook base.yml -e "app_env=$TOPS_DEPLOY_ENV"
